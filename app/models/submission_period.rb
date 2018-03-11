841b8d567824c3d32e433be5911a296e813455b0
Henry Laynesa Jr <henry.laynesa@gmail.com>
2018-03-11 18:15:06 +0800

    added self updating is_active_votation for submission periods

--- app/models/submission_period.rb
+++ app/models/submission_period.rb
@@ -1,9 +1,14 @@
 class SubmissionPeriod < ApplicationRecord
 	belongs_to :proposals, optional: true
 	after_initialize :set_defaults
+	after_initialize :set_is_active_votation
 
 	def set_defaults
 		self.is_set = false if self.is_set.nil?
 		self.is_active_votation = false if self.is_active_votation.nil?
+	end
+
+	def set_is_active_votation
+		self.is_active_votation = true if Time.now >= self.start_votation and Time.now <= self.end_votation
 	end
 end
